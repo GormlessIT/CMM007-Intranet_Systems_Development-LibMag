85cbc4491ddfd4381a8370eeb7791505ee679990
Oliwer Szmytkowski <oliwers2002@gmail.com>
2025-03-13 12:14:42 +0000

    backups moved to Old-Backups branch

--- 20.02 pt.2/mysqli_test.php
+++
@@ -1,8 +0,0 @@
-<?php
-$conn = new mysqli("localhost", "root", "oliSQL112", "LibMag");
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-echo "Connected successfully!";
-?>
