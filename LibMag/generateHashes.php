e35a396bc86f1aa72b2d64d8a21d577fb20a6b81
Oliwer Szmytkowski <oliwers2002@gmail.com>
2025-04-13 11:26:59 +0100

    old backups moved

--- LibMag/generateHashes.php
+++
@@ -1,7 +0,0 @@
-<?php
-$passwords = ['userpass1', 'userpass2', 'adminpass1', 'adminpass2'];
-
-foreach ($passwords as $password) {
-    echo "Plaintext: $password → Hashed: " . password_hash($password, PASSWORD_DEFAULT) . PHP_EOL;
-}
-?>
