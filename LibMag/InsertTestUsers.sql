db54cb902004fdcca2c1e7844eb80eaacbebf646
Oliwer Szmytkowski <oliwers2002@gmail.com>
2025-04-13 11:24:37 +0100

    old backups moved

--- LibMag/InsertTestUsers.sql
+++
@@ -1,8 +0,0 @@
-USE LibMag;
-
-INSERT INTO users (username, email, password, role)
-VALUES 
-('testuser1', 'user1@email.com', 'userpass1', 'user'),
-('testuser2', 'user2@email.com', 'userpass2', 'user'),
-('testadmin1', 'admin@admin.com', 'adminpass1', 'admin'),
-('testadmin2', 'admin2@admin.com', 'adminpass2', 'admin');
